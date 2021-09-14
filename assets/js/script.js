fa803c2cbb6bd975c430b5a0fd8fc99837f702ee
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-09-13 20:36:06 -0500

    added forms and selecter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,11 @@
-var buttonE1 = document.querySelector("#save-task"); //button
+var formE1 = document.querySelector("#task-form") //button
 var tasksToDoE1 = document.querySelector("#tasks-to-do") //Unordered list
 
-buttonE1.addEventListener("click", function() {
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task."
-    tasksToDoE1.appendChild(listItemE1)
-});
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
 };
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
